72ba3649f8b0b91ce1887e7e6c2babb46700e8f5
Sowce <lyoxchan@gmail.com>
2018-12-04 15:17:44 +0100

    first

---
+++ precache-manifest.8cba0516c3eba1a1f39cd919fe43bf16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e531702ab05306313697",
+    "url": "/js/chunk-vendors.16630285.js"
+  },
+  {
+    "revision": "96d7a492a320d5365ee5",
+    "url": "/js/app.fd82595c.js"
+  },
+  {
+    "revision": "78a132c1abc2e5719e47",
+    "url": "/js/about.826b3e79.js"
+  },
+  {
+    "revision": "c34257cda07dfb13da7126cd0ae051d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e531702ab05306313697",
+    "url": "/css/chunk-vendors.f96d6ec3.css"
+  },
+  {
+    "revision": "96d7a492a320d5365ee5",
+    "url": "/css/app.e0bc0a25.css"
+  }
+];
